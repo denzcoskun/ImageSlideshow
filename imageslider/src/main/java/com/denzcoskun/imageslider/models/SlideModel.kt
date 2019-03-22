a416c7e880eb438b633bfa49b96a8a3e771a8b47
Deniz Coskun <deniz.coskun@pia-team.com>
2019-03-22 15:34:54 +0300

    Slide model has been added and project updated with this model.

---
+++ imageslider/src/main/java/com/denzcoskun/imageslider/models/SlideModel.kt
@@ -0,0 +1,30 @@
+package com.denzcoskun.imageslider.models
+
+/**
+ * Created by 34158 on 22.03.2019.
+ */
+class SlideModel {
+
+    var imageUrl: String? = null
+    var imagePath: Int? = 0
+    var title: String? = null
+
+    constructor (imageUrl: String) {
+        this.imageUrl = imageUrl
+    }
+
+    constructor (imagePath: Int) {
+        this.imagePath = imagePath
+    }
+
+    constructor (imageUrl: String, title: String?) {
+        this.imageUrl = imageUrl
+        this.title = title
+    }
+
+    constructor (imagePath: Int, title: String?) {
+        this.imagePath = imagePath
+        this.title = title
+    }
+
+}
